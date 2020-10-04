c16f2012cdd4ae6702e077da8e0c6fd710db79e3
RedMousePad <72360193+RedMousePad@users.noreply.github.com>
2020-10-04 21:24:42 +0000

    Update gh-pages to output generated at 3eedece

--- MangaDex/source.js
+++ MangaDex/source.js
@@ -2603,12 +2603,12 @@ class MangaDex extends paperback_extensions_common_1.Source {
     constructor(cheerio) {
         super(cheerio);
     }
-    get version() { return '1.0.19'; }
-    get name() { return 'MangaDex'; }
+    get version() { return '2.0.0'; }
+    get name() { return 'MangaDex Bypassed'; }
     get icon() { return 'icon.png'; }
-    get author() { return 'Faizan Durrani'; }
-    get authorWebsite() { return 'https://github.com/FaizanDurrani'; }
-    get description() { return 'Extension that pulls manga from MangaDex, includes Advanced Search and Updated manga fetching'; }
+    get author() { return 'RedMousePad'; }
+    get authorWebsite() { return 'https://github.com/RedMousePad'; }
+    get description() { return 'MangaDex repo with licensed manga filter bypassed'; }
     get hentaiSource() { return false; }
     get rateLimit() { return 1; }
     getMangaDetailsRequest(ids) {
